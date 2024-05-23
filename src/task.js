78939d7fe96c40c7b810c3ae6f64e96eb0045949
Gileandro Campos <gileandrocampos8@gmail.com>
2024-05-23 08:52:55 -0300

    feat: add validation from field Description

--- src/task.js
+++ src/task.js
@@ -5,12 +5,15 @@ class Task {
     const task = await taskData;
     const validTitle = this.validTitle(task)
     if(!validTitle.valid) throw new Error(validTitle.error)
-    return validTitle
+    const validDescription = this.validDescription(task)
+    if(!validDescription.valid) throw new Error(validDescription.error)
+    
+    return task
   }
 
   static validTitle(taskData) {
     const containsSpecialCharacters = str => {
-      const regex = /[^a-zA-Z0-9]/;
+      const regex = /[^a-zA-Z0-9 ]/;
       return regex.test(str);
     }
 
@@ -41,9 +44,30 @@ class Task {
         valid: false,
       }
     }
+    return { valid: true }
+  }
 
-    
+  static validDescription(taskData) {
+    if(!taskData.description){
+      return {
+        error: error.description.required,
+        valid: false,
+      }
+    }
 
+    if(taskData.description.length <= 10) {
+      return {
+        error: error.description.tooShort,
+        valid: false,
+      }
+    }
+
+    if(taskData.description.length >= 500) {
+      return {
+        error: error.description.tooLong,
+        valid: false,
+      }
+    }
     return { valid: true }
   }
 
