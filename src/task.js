075a0d48bb9a327996319344c97df816a8ec53c4
Gileandro Campos <gileandrocampos8@gmail.com>
2024-05-25 08:42:10 -0300

    feat: implement due date task validation

--- src/task.js
+++ src/task.js
@@ -7,38 +7,38 @@ class Task {
     if(!validTitle.valid) throw new Error(validTitle.error)
     const validDescription = this.validDescription(task)
     if(!validDescription.valid) throw new Error(validDescription.error)
-    
+    const validDueDate = this.validDate(task)
+    if(!validDueDate.valid) throw new Error(validDueDate.error)
+
     return task
   }
 
   static validTitle(taskData) {
-    const containsSpecialCharacters = str => {
-      const regex = /[^a-zA-Z0-9 ]/;
-      return regex.test(str);
-    }
+    const title = taskData.title
 
-    if(!taskData.title){
+    if(!title){
       return {
         error: error.taskTitle.required,
         valid: false,
       }
     }
 
-    if(taskData.title.length <= 3) {
+    if(title.length <= 3) {
       return {
         error: error.taskTitle.tooShort,
         valid: false,
       }
     }
 
-    if(taskData.title.length >= 100) {
+    if(title.length >= 100) {
       return {
         error: error.taskTitle.tooLong,
         valid: false,
       }
     }
 
-    if(containsSpecialCharacters(taskData.title)) {
+    const containsSpecialCharacters = /[^a-zA-Z0-9 ]/.test(title);
+    if(containsSpecialCharacters) {
       return {
         error: error.taskTitle.invalidCharacter,
         valid: false,
@@ -48,21 +48,22 @@ class Task {
   }
 
   static validDescription(taskData) {
-    if(!taskData.description){
+    const description = taskData.description
+    if(!description){
       return {
         error: error.description.required,
         valid: false,
       }
     }
 
-    if(taskData.description.length <= 10) {
+    if(description.length <= 10) {
       return {
         error: error.description.tooShort,
         valid: false,
       }
     }
 
-    if(taskData.description.length >= 500) {
+    if(description.length >= 500) {
       return {
         error: error.description.tooLong,
         valid: false,
@@ -70,6 +71,45 @@ class Task {
     }
     return { valid: true }
   }
+
+  static validDate(taskData) {
+    const dueDate = taskData.dueDate;
+    const creationDate = taskData.creationDate;
+    
+    if (!dueDate) {
+      return {
+        error: error.dueDate.required,
+        valid: false,
+      };
+    }
+  
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!regex.test(dueDate)) {
+      return {
+        error: error.dueDate.invalid,
+        valid: false,
+      };
+    }
+  
+    const date = new Date(dueDate);
+    const timestamp = date.getTime();
+    if (Number.isNaN(timestamp)) {
+      return {
+        error: error.dueDate.invalid,
+        valid: false,
+      };
+    }
+  
+    if (new Date(dueDate) < new Date(creationDate)) {
+      return {
+        error: error.dueDate.pastDate,
+        valid: false,
+      };
+    }
+  
+    return { valid: true };
+  }
+  
 
 }
 
