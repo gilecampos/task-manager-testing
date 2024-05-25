31d22f0bbc164b1eb86567b928580440681b7067
Gileandro Campos <gileandrocampos8@gmail.com>
2024-05-25 08:42:35 -0300

    test: implement test in dueDate task

--- index.test.js
+++ index.test.js
@@ -88,4 +88,46 @@ const assert = require('assert')
     await assert.rejects(result, expected)
   }
 
+  {
+    const task = {
+      id: faker.string.uuid(),
+      title: 'Title task',
+      description: 'Uma descrição pra lá de boa',
+      dueDate: '',
+      creationDate: new Date(),
+    }
+    
+    const result = Task.ToDoList(task)
+    const expected = new Error(error.dueDate.required);
+    await assert.rejects(result, expected)
+  }
+
+  {
+    const task = {
+      id: faker.string.uuid(),
+      title: 'Title task',
+      description: 'Uma descrição pra lá de boa',
+      dueDate: '10-09-2024',
+      creationDate: new Date(),
+    }
+    
+    const result = Task.ToDoList(task)
+    const expected = new Error(error.dueDate.invalid);
+    await assert.rejects(result, expected)
+  }
+
+  {
+    const task = {
+      id: faker.string.uuid(),
+      title: 'Title task',
+      description: 'Uma descrição pra lá de boa',
+      dueDate: '2024-02-20',
+      creationDate: new Date(),
+    }
+    
+    const result = Task.ToDoList(task)
+    const expected = new Error(error.dueDate.pastDate);
+    await assert.rejects(result, expected)
+  }
+
 })()
